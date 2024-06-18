2c4c4bb297311fcd7193dcd03d76d85f9d481054
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-06-18 17:51:10 -0300

    refactors exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { range } from "../../utils";
-import { checkGuess } from "../../game-helpers";
+import React from 'react';
+
+import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
+
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+
+  return <span className={className}>{letter}</span>;
+}
 
 function Guess({ value, answer }) {
-  const checkedValue = checkGuess(value, answer);
+  const result = checkGuess(value, answer);
 
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span
+        <Cell
           key={num}
-          className={`cell ${value && checkedValue[num].status}`}
-        >
-          {value ? value[num] : " "}
-        </span>
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
   );
